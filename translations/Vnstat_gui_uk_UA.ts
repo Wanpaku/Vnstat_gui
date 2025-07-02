c286dc567588e963bf835c9fee2557ab5943bb9d
Teg Miles <135981466+Wanpaku@users.noreply.github.com>
2025-07-02 22:35:09 +0300

    Add translations

---
+++ translations/Vnstat_gui_uk_UA.ts
@@ -0,0 +1,219 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="uk_UA" sourcelanguage="en_US">
+<context>
+    <name>Logger</name>
+    <message>
+        <location filename="../logger.cpp" line="61"/>
+        <source>Couldn&apos;t open file </source>
+        <translation type="unfinished">Не зміг відкрити файл </translation>
+    </message>
+    <message>
+        <location filename="../logger.cpp" line="81"/>
+        <source>line: </source>
+        <translation type="unfinished">рядок: </translation>
+    </message>
+    <message>
+        <location filename="../logger.cpp" line="83"/>
+        <source>message: </source>
+        <translation type="unfinished">повідомлення: </translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../mainwindow.ui" line="23"/>
+        <source>Interface</source>
+        <translation type="unfinished">Інтерфейс</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="32"/>
+        <location filename="../mainwindow.ui" line="54"/>
+        <source>None</source>
+        <translation type="unfinished">Немає</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="42"/>
+        <source>Time period</source>
+        <translation type="unfinished">Часовий проміжок</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="64"/>
+        <source>Units</source>
+        <translation type="unfinished">Одиниці вимірювання</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="111"/>
+        <source>Date</source>
+        <translation type="unfinished">Дата</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="146"/>
+        <source>File</source>
+        <translation type="unfinished">Файл</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="152"/>
+        <source>Settings</source>
+        <translation type="unfinished">Налаштування</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="156"/>
+        <source>Interface_language</source>
+        <translation type="unfinished">Мова інтерфейсу</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="163"/>
+        <source>Help</source>
+        <translation type="unfinished">Допомога</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="177"/>
+        <source>Quit</source>
+        <translation type="unfinished">Вихід</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="180"/>
+        <source>Ctrl+Q</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="188"/>
+        <source>About</source>
+        <translation type="unfinished">Про додаток</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="191"/>
+        <source>F1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="11"/>
+        <source>Start program Vnstat_gui.</source>
+        <translation type="unfinished">Запуск програми Vnstat_gui.</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="33"/>
+        <source>Exit from the app</source>
+        <translation type="unfinished">Вихід з програми</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="97"/>
+        <source>Interface language changed to: </source>
+        <translation type="unfinished">Мова інтерфейсу змінена на: </translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="156"/>
+        <source>Error: Could not open vnstat database file</source>
+        <translation type="unfinished">Не зміг відкрити файл бази даних vnstat</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="158"/>
+        <source>Database Error</source>
+        <translation type="unfinished">Помилка бази даних</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="159"/>
+        <source>Could not open database file: </source>
+        <translation type="unfinished">Не зміг відкрити файл бази даних: </translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="289"/>
+        <source>Top days</source>
+        <translation type="unfinished">Найбільше на день</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="116"/>
+        <source>Received</source>
+        <translation type="unfinished">Отримано</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="121"/>
+        <source>Transmitted</source>
+        <translation type="unfinished">Передано</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="126"/>
+        <source>Total</source>
+        <translation type="unfinished">Загалом</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="287"/>
+        <source>5th minutes</source>
+        <translation type="unfinished">5-ть хвилин</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="287"/>
+        <source>Hour</source>
+        <translation type="unfinished">Година</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="288"/>
+        <source>Day</source>
+        <translation type="unfinished">День</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="288"/>
+        <source>Month</source>
+        <translation type="unfinished">Місяць</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="289"/>
+        <source>Year</source>
+        <translation type="unfinished">Рік</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="303"/>
+        <source>Bytes</source>
+        <translation type="unfinished">Байти</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="303"/>
+        <source>Kilobytes</source>
+        <translation type="unfinished">Кілобайти</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="304"/>
+        <source>Megabytes</source>
+        <translation type="unfinished">Мегабайти</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="304"/>
+        <source>Gigabytes</source>
+        <translation type="unfinished">Гігабайти</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="356"/>
+        <source>&lt;b&gt;&lt;p&gt;&lt;center&gt;Vnstat_gui&lt;/b&gt;&lt;/p&gt;&lt;/center&gt;&lt;p&gt;&lt;center&gt;This is a simple program that shows network statistics. The data loading from vnstat database file. Therefore you need to have installed vnstat application on yours PC. &lt;/p&gt;&lt;/center&gt;&lt;p&gt;&lt;center&gt;The app icon created by &lt;a href=&apos;https://www.flaticon.com/free-icon/setting_7415329&apos;&gt;Eucalyp - Flaticon&lt;/a&gt;&lt;/p&gt;&lt;/center&gt;&lt;p&gt;&lt;center&gt;License: &lt;a href=&apos;https://www.gnu.org/licenses/gpl-3.0.html&apos;&gt;GNU Public License v3&lt;/a&gt;&lt;/p&gt;&lt;/center&gt;&lt;p&gt;&lt;center&gt;Copyright 2025 ©Teg Miles (movarocks2@gmail.com)&lt;/p&gt;&lt;/center&gt;</source>
+        <translation type="unfinished">&lt;b&gt;&lt;p&gt;&lt;center&gt;Vnstat_gui&lt;/b&gt;&lt;/p&gt;&lt;/center&gt;&lt;p&gt;&lt;center&gt;Це проста програма, що показує мережеву статистику. Дані завантажуються з файлу бази даних vnstat. Тому на вашому ПК має бути встановлено програму vnstat. &lt;/p&gt;&lt;/center&gt;&lt;p&gt;&lt;center&gt;Іконка додатку створена: &lt;a href=&apos;https://www.flaticon.com/free-icon/setting_7415329&apos;&gt;Eucalyp - Flaticon&lt;/a&gt;&lt;/p&gt;&lt;/center&gt;&lt;p&gt;&lt;center&gt;Ліцензія: &lt;a href=&apos;https://www.gnu.org/licenses/gpl-3.0.html&apos;&gt;GNU Public License v3&lt;/a&gt;&lt;/p&gt;&lt;/center&gt;&lt;p&gt;&lt;center&gt;Copyright 2025 ©Teg Miles (movarocks2@gmail.com)&lt;/p&gt;&lt;/center&gt;</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="371"/>
+        <source>Watched About info</source>
+        <translation type="unfinished">Проглянуто інформацію про програму</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../logger.cpp" line="44"/>
+        <source>Info</source>
+        <translation type="unfinished">Інформація</translation>
+    </message>
+    <message>
+        <location filename="../logger.cpp" line="45"/>
+        <source>Warning</source>
+        <translation type="unfinished">Попередження</translation>
+    </message>
+    <message>
+        <location filename="../logger.cpp" line="46"/>
+        <source>Critical</source>
+        <translation type="unfinished">Критичне попередження</translation>
+    </message>
+    <message>
+        <location filename="../logger.cpp" line="47"/>
+        <source>Fatal</source>
+        <translation type="unfinished">Фатальне попередження</translation>
+    </message>
+</context>
+</TS>
