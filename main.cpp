f353dfc2c5871535412f8436c4dbc6846f102e82
Teg Miles <135981466+Wanpaku@users.noreply.github.com>
2025-07-02 22:53:32 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,3 +1,19 @@
+/*Copyright (C) 2025  Teg Miles
+ This file is part of Vnstat_gui.
+
+Movar is free software: you can redistribute it and/or modify it
+under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License,
+or any later version.
+
+Movar is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty
+of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Vnstat_gui. If not, see <https://www.gnu.org/licenses/>.*/
+
 #include "mainwindow.h"
 #include "logger.h"
 
